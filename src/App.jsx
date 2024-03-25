b5c171ff0e78c7c687d8444627b53da72add62f9
brenda-soto <bsoto@pursuit.org>
2024-03-24 23:44:38 -0400

    Routes made

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import ProtectedRoute from "./Components/Authorization/ProtectedRoute";
 import Register from "./Components/Authorization/Register";
 import Login from "./Components/Authorization/Login";
 import Dashboard from "./Components/Authorization/Dashboard";
+import ReviewForm from "./Components/Reviews/ReviewForm";
+import Index from "./Pages/Index";
+import Show from "./Pages/Show";
+import FourOFour from "./Pages/FourOFour";
+import About from "./Pages/About";
+import LandingPage from "./Pages/LandingPage";
 
 function App() {
   return (
@@ -11,19 +17,26 @@ function App() {
       <Route
         path="/"
         element={
-          <div>
-            <h1>Welcome to the Auth Starter</h1>
-            <Link to="/login">Login</Link>
-            <h2>If you are not logged in you cannot reach this route. Try!</h2>
-            <Link to="/dashboard">Dashboard</Link>
-          </div>
+          <LandingPage />
+          // <div>
+          //   <h1>Welcome to the Auth Starter</h1>
+          //   <Link to="/login">Login</Link>
+          //   <h2>If you are not logged in you cannot reach this route. Try!</h2>
+          //   <Link to="/dashboard">Dashboard</Link>
+          // </div>
         }
       />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      {/* Names of routes? */}
+      <Route path="/teapots" element={<Index />} />
+      <Route path="/teapots/:id/reviews" element={<Show />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<FourOFour />} />
       <Route element={<ProtectedRoute />}>
         {/* Place protected routes here */}
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/form" element={<ReviewForm />} />
       </Route>
     </Routes>
   );
