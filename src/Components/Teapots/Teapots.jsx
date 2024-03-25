c6d9ab6fc1f06d176d2f03692f1f30f398aa2dc4
AnitaOwen <anitaowen@pursuit.org>
2024-03-25 11:23:25 -0400

    fetch the teapots data in the index page

--- src/Components/Teapots/Teapots.jsx
+++ src/Components/Teapots/Teapots.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { useState, useEffect  } from "react";
+import Teapot from "../Teapots/Teapot.jsx"
+const URL = import.meta.env.VITE_BASE_URL;
 
 const Teapots = () => {
-  return <div>Teapots</div>;
+  const [teapots, setTeapots] = useState([])
+  
+  useEffect(() => {
+    fetch(`${URL}/api/teapots`)
+    .then((res) => res.json())
+    .then((data) => setTeapots(data))
+    .catch((error) => console.error(error))
+  }, [])
+
+  return (
+    <div>
+      Teapots
+      {teapots.map((teapot) => {
+        return <Teapot key={teapot.id} teapot={teapot} />
+      })}
+    </div>
+  )
 };
 
 export default Teapots;
--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Teapots from "../Components/Teapots/Teapots.jsx"
 
 const Index = () => {
-  return <div>Index</div>;
+  return (
+    <div>
+      <Teapots />
+    </div>
+  )
 };
 
 export default Index;
